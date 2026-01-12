983bdbe5c9b2b52931b6c5bb8533890b45e107cb
Bartek Włodarczyk <wlodarcykb16@gmail.com>
2026-01-12 16:19:06 +0100

    poprawka

--- kernels.py
+++ kernels.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-# Wykrywanie krawędzi - Operatory Sobela [cite: 10]
+# Wykrywanie krawędzi - Operatory Sobela 
 SOBEL_X = np.array([
     [1, 0, -1],
     [2, 0, -2],
@@ -13,28 +13,28 @@ SOBEL_Y = np.array([
     [-1, -2, -1]
 ])
 
-# Wykrywanie krawędzi - Operator Laplace'a [cite: 10]
+# Wykrywanie krawędzi - Operator Laplace'a 
 LAPLACE = np.array([
     [0, 1, 0],
     [1, -4, 1],
     [0, 1, 0]
 ])
 
-# Rozmywanie - Jądro Gaussowskie G [cite: 18]
+# Rozmywanie - Jądro Gaussowskie G 
 GAUSSIAN = (1/16) * np.array([
     [1, 2, 1],
     [1, 4, 1],
     [1, 2, 1]
 ])
 
-# Wyostrzanie - Jądro W [cite: 24]
+# Wyostrzanie - Jądro W 
 SHARPEN = np.array([
     [0, -1, 0],
     [-1, 5, -1],
     [0, -1, 0]
 ])
 
-# Bonus: Operator Prewitta (Oś X) [cite: 63, 76]
+# Bonus: Operator Prewitta (Oś X) 
 PREWITT_X = np.array([
     [1, 0, -1],
     [1, 0, -1],
